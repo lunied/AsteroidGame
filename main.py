613b23ba0eb81f893997afba35bce13f3720ae70
lunied <lukas.niederlohmann@gmail.com>
2025-11-28 16:03:01 +0100

    Added player

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
+import player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    the_player = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while(True):
         log_state()
@@ -12,7 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        the_player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
